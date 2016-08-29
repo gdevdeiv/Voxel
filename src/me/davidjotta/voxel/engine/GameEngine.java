390010a59a95ce4dab608b144b69d71bf019b6bc
David Juan Ahullana <info@davidjotta.me>
2016-08-29 18:52:11 +0200

    Initial commit

---
+++ src/me/davidjotta/voxel/engine/GameEngine.java
@@ -0,0 +1,87 @@
+package me.davidjotta.voxel.engine;
+
+public class GameEngine implements Runnable {
+	
+	public static final int TARGET_FPS = 75;
+	public static final int TARGET_UPS = 30;
+
+	private final Thread gameLoopThread;
+	private final Window window;
+	private final IGameLogic gameLogic;
+	private final Timer timer;
+	
+	public GameEngine(String windowTitle, int width, int height, boolean vsSync, IGameLogic gameLogic) throws Exception {
+		gameLoopThread = new Thread(this, "GAME_LOOP_THREAD");
+		window = new Window(windowTitle, width, height, vsSync);
+		this.gameLogic = gameLogic;
+		timer = new Timer();
+	}
+	
+	public void start() {
+		String osName = System.getProperty("os.name");
+		if (osName.contains("Mac")) {
+			gameLoopThread.run();
+		} else {
+			gameLoopThread.start();
+		}
+	}
+
+	@Override
+	public void run() {
+		try {
+			init();
+			gameLoop();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	protected void init() throws Exception {
+		window.init();
+		timer.init();
+		gameLogic.init();
+	}
+	
+	protected void gameLoop() {
+		float elapsedTime;
+		float accumulator = 0f;
+		float interval = 1f / TARGET_UPS;
+		boolean running = true;
+		while (running && !window.windowShouldClose()) {
+			elapsedTime = timer.getElapsedTime();
+			accumulator += elapsedTime;
+			input();
+			while (accumulator >= interval) {
+				update(interval);
+				accumulator -= interval;
+			}
+			render();
+			if (!window.isvSync()) {
+				sync();
+			}
+		}
+	}
+	
+	private void sync() {
+		float loopSlot = 1f / TARGET_FPS;
+		double endTime = timer.getLastLoopTime() + loopSlot;
+		while (timer.getTime() < endTime) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException ex) {}
+		}
+	}
+	
+	protected void input() {
+		gameLogic.input(window);
+	}
+	
+	protected void update(float interval) {
+		gameLogic.update(interval);
+	}
+	
+	protected void render() {
+		gameLogic.render(window);
+		window.update();
+	}
+}
